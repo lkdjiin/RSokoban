b206b1fa059275642f0b90e7f077f5614ba1fed1
lkdjiin <xavier.nayrac@gmail.com>
2011-01-21 09:53:46 +0100

    change location of test.rb

---
+++ test/test.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require "rsokoban"
+
+require "test/unit"
+
+require "../test/tc_level"
+require "../test/tc_level_loader"
+require "../test/tc_level_set"
+require "../test/tc_raw_level"
+require "../test/tc_position"
+require "../test/tc_moveable"
+require "../test/tc_man"
+require "../test/ui/tc_console"
+
