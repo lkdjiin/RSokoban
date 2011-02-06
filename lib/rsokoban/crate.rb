f09916c9370a388eae94e30e92210148d506fc23
lkdjiin <xavier.nayrac@gmail.com>
2011-02-06 11:00:39 +0100

    refactor crate.rb

--- lib/rsokoban/crate.rb
+++ lib/rsokoban/crate.rb
@@ -2,14 +2,13 @@ require "rsokoban/moveable"
 
 module RSokoban
 
-	# I am a moveable crate.
+	# I am a positionable, moveable crate.
 	class Crate < Position
 		include RSokoban::Moveable
 		
-		# @param [Fixnum] x la coordonnée x
-		# @param [Fixnum] y la coordonnée y
-		def initialize x, y
-			super(x, y)
+		# Coordinates must be Fixnum
+		def initialize x_coord, y_coord
+			super(x_coord, y_coord)
 		end
 	end
 	
