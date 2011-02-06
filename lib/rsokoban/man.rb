7746835f92253131dceb8aeab1208b3984135f50
lkdjiin <xavier.nayrac@gmail.com>
2011-02-06 11:22:34 +0100

    refactor man.rb

--- lib/rsokoban/man.rb
+++ lib/rsokoban/man.rb
@@ -6,10 +6,9 @@ module RSokoban
 	class Man < Position
 		include RSokoban::Moveable
 		
-		# @param [Fixnum] x la coordonnée x
-		# @param [Fixnum] y la coordonnée y
-		def initialize x, y
-			super(x, y)
+		# Coordinates must be Fixnum
+		def initialize x_coord, y_coord
+			super(x_coord, y_coord)
 		end
 		
 	end
--- lib/rsokoban/position.rb
+++ lib/rsokoban/position.rb
@@ -5,6 +5,7 @@ module RSokoban
 	class Position
 		attr_reader :x, :y
 		
+		# Coordinates must be Fixnum
 		def initialize x = 0, y = 0
 			@x = x
 			@y = y
