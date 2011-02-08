b2cfaadf56d02c6a83f66b81a3708bfdcfdc4099
lkdjiin <xavier.nayrac@gmail.com>
2011-02-08 10:34:31 +0100

    refactor curses_console.rb

--- lib/rsokoban/ui/curses_console.rb
+++ lib/rsokoban/ui/curses_console.rb
@@ -4,11 +4,11 @@ module RSokoban::UI
 	# I assume 24 lines height.
 	class CursesConsole < BaseUI
 	
-		@@SET_LINE = 0
-		@@TITLE_LINE = 1
-		@@MOVES_LINE = 2
-		@@STATUS_LINE = 3
-		@@PICTURE_LINE = 5
+		SET_LINE = 0
+		TITLE_LINE = 1
+		MOVES_LINE = 2
+		STATUS_LINE = 3
+		PICTURE_LINE = 5
 		
 		def initialize
 			super()
@@ -18,13 +18,7 @@ module RSokoban::UI
 		end
 		
 		def get_action(hash)
-			if hash[:type] == :start
-				@level_title = hash[:title]
-				@set_title = hash[:set]
-				@level_number = hash[:number]
-				@set_total = hash[:total]
-				Curses.clear
-			end
+			initialize_members_and_clear_screen hash
 			if [:display, :start].include?(hash[:type]) 
 				ask_player hash
 			else
@@ -35,6 +29,16 @@ module RSokoban::UI
 		
 		private
 		
+		def initialize_members_and_clear_screen hash
+			if hash[:type] == :start
+				@level_title = hash[:title]
+				@set_title = hash[:set]
+				@level_number = hash[:number]
+				@set_total = hash[:total]
+				Curses.clear
+			end
+		end
+		
 		def init_screen
 			Curses.noecho # do not show typed keys
 			Curses.init_screen
@@ -43,12 +47,20 @@ module RSokoban::UI
 		end
 		
 		def display hash
-			write @@SET_LINE, 0, "Set: #{@set_title}"
-			write @@TITLE_LINE, 0, "Level: #{@level_title} (#{@level_number}/#{@set_total})"
-			write @@MOVES_LINE, 0, "moves : #{hash[:move].to_s}   " unless hash[:move].nil?
-			write @@STATUS_LINE, 0, 'arrows=move (q)uit (r)etry (u)ndo (l)oad level/set'
-			line_num = @@PICTURE_LINE
-			hash[:map].each {|line| 
+			display_header hash[:move]
+			display_map hash[:map]
+		end
+		
+		def display_header move_number
+			write SET_LINE, 0, "Set: #{@set_title}"
+			write TITLE_LINE, 0, "Level: #{@level_title} (#{@level_number}/#{@set_total})"
+			write MOVES_LINE, 0, "moves : #{move_number}   " unless move_number.nil?
+			write STATUS_LINE, 0, 'arrows=move (q)uit (r)etry (u)ndo (l)oad level/set'
+		end
+		
+		def display_map map
+			line_num = PICTURE_LINE
+			map.each {|line| 
 				write line_num, 0, line
 				line_num += 1
 			}
@@ -61,7 +73,7 @@ module RSokoban::UI
 		
 		def ask_for_next_level hash
 			display hash
-			write @@STATUS_LINE, 0, "LEVEL COMPLETED ! Play next level ? (yes, no)        "
+			write STATUS_LINE, 0, "LEVEL COMPLETED ! Play next level ? (yes, no)        "
 			case Curses.getch
 				when ?n, ?N then PlayerAction.new(:quit)
 				when ?y, ?Y then PlayerAction.new(:next)
@@ -96,8 +108,34 @@ module RSokoban::UI
 		end
 		
 		def ask_level_or_set
+			show_cursor
+			display_help
+			response = Curses.getstr
+			hide_cursor
+			case response
+				when '1'..'999'
+					if response.to_i > @set_total
+						ask_level_or_set
+					else
+						PlayerAction.new(response.to_i)
+					end
+				when /\.xsb$/ then PlayerAction.new(response)
+				else
+					PlayerAction.new(:retry)
+			end
+		end
+		
+		def show_cursor
 			Curses.curs_set 1
 			Curses.echo
+		end
+		
+		def hide_cursor
+			Curses.curs_set 0
+			Curses.noecho
+		end
+		
+		def display_help
 			Curses.clear
 			help=<<EOS
 ------------------------------------------------------------
@@ -109,20 +147,6 @@ Int 2 : type any letter to cancel and restart previous level
 
 EOS
 			write 0, 0, help
-			str = Curses.getstr
-			Curses.curs_set 0
-			Curses.noecho
-			case str
-				when '1'..'999'
-					if str.to_i > @set_total
-						ask_level_or_set
-					else
-						PlayerAction.new(str.to_i)
-					end
-				when /\.xsb$/ then PlayerAction.new(str)
-				else
-					PlayerAction.new(:retry)
-			end
 		end
 		
 	end
